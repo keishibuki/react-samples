98084edc3c8752799c88675be84a8b52bdfe24f7
Kei <shibuki@cunelwork.co.jp>
2020-02-28 11:50:06 +0900

    Add: react-slick sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
+import ReactSlick from "./react-slick";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Samples</h1>
+      <section>
+        <ReactSlick />
+      </section>
     </div>
   );
 }
---
+++ src/react-slick/index.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const reactSlick = () => {
+  return (
+    <div>
+      <h2>React Slick</h2>
+      <Slider slidesToShow={1} slidesToScroll={1} speed={500} dots infinite>
+        <div>a</div>
+        <div>b</div>
+        <div>c</div>
+      </Slider>
+    </div>
+  );
+};
+
+export default reactSlick;
