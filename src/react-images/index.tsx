152b765332c9f70f4b26bd60b93dd88aa6d57d03
Kei <shibuki@cunelwork.co.jp>
2020-04-03 09:30:29 +0900

    Add: react-images

--- src/react-images/index.tsx
+++ src/react-images/index.tsx
@@ -1,5 +1,54 @@
 import * as React from "react";
+import Carousel, { Modal, ModalGateway } from "react-images";
 
 export default () => {
-  return <div>React Images</div>;
+  const [modalIndex, setModalIndex] = React.useState(0);
+  const [modalIsOpen, toggleModal] = React.useState(false);
+  const images = [
+    {
+      source: "https://placehold.jp/cccccc/888888/150x150.png?text=Sample1",
+      caption: "Smaple1"
+    },
+    {
+      source: "https://placehold.jp/cccccc/888888/150x150.png?text=Sample2",
+      caption: "Sample2"
+    },
+    {
+      source: "https://placehold.jp/cccccc/888888/150x150.png?text=Sample3",
+      caption: "Sample3"
+    }
+  ];
+
+  return (
+    <div>
+      <div>
+        {images.map((image, i) => (
+          <div
+            key={image.source}
+            onClick={() => {
+              setModalIndex(i);
+              toggleModal(true);
+            }}
+          >
+            <img src={image.source} alt={image.caption} />
+          </div>
+        ))}
+      </div>
+      <ModalGateway>
+        {modalIsOpen ? (
+          <Modal
+            closeOnBackdropClickBoolean
+            allowFullscreen={false}
+            onClose={() => toggleModal(false)}
+          >
+            <Carousel
+              currentIndex={modalIndex}
+              frameProps={{ autoSize: "height" }}
+              views={images}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
+  );
 };
